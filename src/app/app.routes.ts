ee7b6b6797b56de2cb9e67833f0e0cf5b21b5943
PabloGarcia <pablo.garcia366@gmail.com>
2019-07-15 09:27:11 -0300

    header login register

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,6 +7,7 @@ import { LoginComponent } from './login/login.component';
 import { ProgressComponent } from './pages/progress/progress.component';
 import { Graficas1Component } from './pages/graficas1/graficas1.component';
 import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
+import { RegisterComponent } from './login/register.component';
 
 
 
@@ -23,7 +24,7 @@ const appRoutes: Routes = [
     ]
   },
   { path: 'login', component: LoginComponent },
-  { path: 'register', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
   { path: '**', component: NopagefoundComponent }
 ];
 
